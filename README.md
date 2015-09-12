00b38f3bea9f644a5279486ea2f99a04f54d0af8
islas27 <islas.27@gmail.com>
2015-09-12 00:26:03 -0600

    Creación del proyecto y dependencias
    Se crea el proyecto, se añaden las dependencias a utilizar y se hace la
    estructura básica del proyecto.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # proyecto1-draw
 Proyecto de evaluación, primer parcial
+
+
+Requisitos:
+Java 8
+
+Manejador de base de datos: MySQL
+Usuario: "usuario"
+Password: ""
+Base de datos: hwrace (UTF-8 para el soporte a los acentos en español)
---
+++ almacenesHwrace/src/main/java/mx/uach/fing/almaceneshwrace/handlers/Home.java
@@ -0,0 +1,16 @@
+package mx.uach.fing.almaceneshwrace.handlers;
+
+/**
+ * Main class of the project. From here spark is going to hace control 
+ * of the routes of the web aplication.
+ * Created Sept 11 @ 11:38
+ * @author Jonathan
+ * @version 1.0
+ */
+public class Home {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
