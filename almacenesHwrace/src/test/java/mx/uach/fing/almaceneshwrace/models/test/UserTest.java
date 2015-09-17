d628b244f5643ee5fc4b7511e7116be225be61e9
chuvacagapj@hotmail.com <Jesus Garcia>
2015-09-16 21:35:45 -0600

    Se agregan pruevas unitarias y javadoc a la clase User

--- almacenesHwrace/src/test/java/mx/uach/fing/almaceneshwrace/models/test/UserTest.java
+++ almacenesHwrace/src/test/java/mx/uach/fing/almaceneshwrace/models/test/UserTest.java
@@ -12,6 +12,7 @@ import javax.persistence.Persistence;
 import javax.persistence.Query;
 import static mx.uach.fing.almaceneshwrace.models.ActiveRecord.PU;
 import mx.uach.fing.almaceneshwrace.models.User;
+import org.eclipse.persistence.exceptions.DatabaseException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -43,18 +44,80 @@ public class UserTest {
     
     @After
     public void tearDown() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory(PU);
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        Query q = em.createQuery("DELETE User u");
+        q.executeUpdate();
+        em.getTransaction().commit();
+        em.close();
     }
 
-    @Test
+    @Test 
     public void createUserTest(){
+        User u = new User();
+        
+        u.setEmail("Iperez@ejemplo.com");
+        u.setName("Iban Perez");
+        u.setPassword("la super secreta clave");
+        u.setIsAdmin(Boolean.FALSE);
+        u.create();
+        
+        assertEquals(1, User.findByEmail("chuy@ejemplo.com").size());
+    }
+    
+    @Test ( expected =  DatabaseException.class)
+    public void createUserTestFail(){
         User u = new User();
         u.setEmail("chuy@ejemplo.com");
         u.setName("Jesus Garcia");
         u.setPassword("la super secreta clave");
         u.setIsAdmin(Boolean.FALSE);
+        u.create();
         
         u.create();
         
         assertEquals(1, User.findByEmail("chuy@ejemplo.com").size());
+    }
+    
+    @Test
+    public void UpdateUserTest(){
+        User u = new User();
+        
+        u.setEmail("chuy@ejemplo.com");
+        u.setName("Jesus Garcia");
+        u.setPassword("la super secreta clave");
+        u.setIsAdmin(Boolean.FALSE);
+        u.create();
+        
+        u.setPassword("La super clave indesifrable");
+        
+        assertSame("la super secreta clave", User.findByEmail("chuy@ejemplo.com").getEmail());
+        
+        u.update();
+        
+        assertSame("La super clave indesifrable", User.findByEmail("chuy@ejemplo.com").getEmail());
+    }
+    
+    @Test ( expected =  DatabaseException.class)
+    public void UpdateUserFail(){
+        User u = new User();
+        User v = new User();
+        
+        u.setEmail("chuy@ejemplo.com");
+        u.setName("Jesus Garcia");
+        u.setPassword("la super secreta clave");
+        u.setIsAdmin(Boolean.FALSE);
+        u.create();
+        
+        v.setEmail("jgarcia@ejemplo.com");
+        v.setName("Jorge Garcia");
+        v.setPassword("contraseña");
+        v.setIsAdmin(Boolean.FALSE);
+        v.create();
+        
+        u.setEmail("jgarcia@ejemplo.com");
+        u.update();
     }
 }
